16ec2409e2dbe2d29f5591e26f455169752f7c25
Conner Smith <conner@smith.net>
2024-06-03 12:46:22 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+# These are placeholders (ie these are what I used when making this), so replace these with your own machine specs.
 USER="Conner"
-DEVICE="Desktop"
-DISTRO="Debian Linux"
-MODEL="Custom Build PC"
+DEVICE="MacBook"
+DISTRO="Ubuntu Linux"
+MODEL="MacBook Pro 13-inch (Early 2015)"
+# Automatically grab the amount of megabytes of RAM. If you'd like to lie-- I mean manually specify the amount of RAM, comment this line and uncomment the one below it.
 RAM=$(($(getconf _PHYS_PAGES) * $(getconf PAGE_SIZE) / (1024 * 1024)))"M"
-CITY="Plano"
+#RAM="32036M"
+CITY="Dallas"
+
+# Path to where you want to keep the wtr and moon files
+wtr_path="/home/conner/.scripts/wtr"
+moon_path="/home/conner/.scripts/moon"
+
 # This part creates the color bars. Feel free to change the numbers, this is your software now :)
 clear
 echo -e "\e[101m          \e[0m"
@@ -50,7 +58,7 @@ clear
 echo -e "\e[101m          \e[0m   $USER's $DEVICE"
 echo -e "\e[103m        \e[0m     $DISTRO"
 echo -e "\e[102m      \e[0m       "
-echo -e "\e[106m    \e[0m         $DEVICE"
+echo -e "\e[106m    \e[0m         $MODEL"
 echo -e "\e[105m  \e[0m           $RAM OK"
 
 echo "Hello, $USER."
@@ -60,10 +68,6 @@ TIMENOW=$(date +"The local time is %r")
 echo $TODAY
 echo $TIMENOW
 echo
-
-# Path to where you want to keep the wtr and moon files
-wtr_path="/home/conner/.scripts/wtr"
-moon_path="/home/conner/.scripts/moon"
 
 # Checking if the wtr and moon files exist
 if [ -e "$wtr_path" ]; then
