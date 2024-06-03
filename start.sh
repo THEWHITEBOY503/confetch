2f475d106ce8f3ceb507a8bde33f183b3cf66c97
Conner Smith <conner@smith.net>
2024-06-03 12:53:31 -0500

    Making variables for wttr.in options

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# These are placeholders (ie these are what I used when making this), so replace these with your own machine specs.
+# Configure your options here. These are the options I used on my MacBook, provided as an example/placeholder.
 USER="Conner"
 DEVICE="MacBook"
 DISTRO="Ubuntu Linux"
-MODEL="MacBook Pro 13-inch (Early 2015)"
+MODEL="Custom Build PC"
+WTROPTIONS="2FnQ"
+MOONOPTIONS="F"
 # Automatically grab the amount of megabytes of RAM. If you'd like to lie-- I mean manually specify the amount of RAM, comment this line and uncomment the one below it.
 RAM=$(($(getconf _PHYS_PAGES) * $(getconf PAGE_SIZE) / (1024 * 1024)))"M"
 #RAM="32036M"
-CITY="Dallas"
+CITY="Plano"
 
 # Path to where you want to keep the wtr and moon files
 wtr_path="/home/conner/.scripts/wtr"
@@ -73,29 +75,30 @@ echo
 if [ -e "$wtr_path" ]; then
     # Get the wtr file's modification time in seconds
     modification_time=$(date -r "$wtr_path" "+%s")
-    
+
     # Get the current time in second
     current_time=$(date "+%s")
-    
+
     # Calculate the time difference in seconds
     time_difference=$((current_time - modification_time))
-    
+
     # Check if the file is older than 12 hours (43200 seconds)
     if [ "$time_difference" -gt 43200 ]; then
         # If the time is different, download a new copy of the file
-        curl -s wttr.in/$CITY?2FnQ > wtr
-        curl -s wttr.in/moon?F > moon
+        curl -s wttr.in/$CITY?$WTROPTIONS > $wtr_path
+        curl -s wttr.in/moon?$MOONOPTIONS > $moon_path
+        # Easter egg-- Hello from the dev! Thanks for taking a look at my (shitty) software. <3 -CS
     else
         # The file's already new enough, so we don't need to download an updated version.
-        # Remove the &>/dev/null if you want it to log on the splash screen (for whatever reason)
+        # Remove the &>/dev/null if you want it to log on the splash screen (for diagnostics, I guess)
         echo "File is already new." &>/dev/null
     fi
 else
     # File not found, so downloading the file.
-    # Remove the &>/dev/null if you want it to log on the splash screen (for whatever reason)
+    # Remove the &>/dev/null if you want it to log on the splash screen (for diagnostics)
     echo "Downloading new moon and wtr files" &>/dev/null
-    curl -s wttr.in/$CITY?2FnQ > $wtr_path
-    curl -s wttr.in/moon?F > $moon_path
+    curl -s wttr.in/$CITY?$WTROPTIONS > $wtr_path
+    curl -s wttr.in/moon?$MOONOPTIONS > $moon_path
 fi
 echo "Here's the weather in $CITY:"
 # Display the content of the files side by side
